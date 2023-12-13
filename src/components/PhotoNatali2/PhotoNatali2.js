e3b3aaca80f58045cf5715e29d4cb948caca021c
Владимир <davidvovchik@gmail.com>
2023-12-13 16:46:05 +0200

    www

--- src/components/PhotoNatali2/PhotoNatali2.js
+++ src/components/PhotoNatali2/PhotoNatali2.js
@@ -79,41 +79,40 @@ const PhotoNatali2 = () => {
         <div className='page2__natali__wrap'>
             <div className='page2__wrap__card'></div>
             <div className='page2__main_adapt'><div className='page2__border__adapt'></div></div>
-            <div className='page2__vertical__adapt'>
-                <div className='page2__photo__natali'></div>
-                <div className='page2__card__border'></div>
-                <div className='page2__float__photo'>
-                    <ul className='page2__profile__wrap'>
-                        <li>
-                            <ul className='page2__profile__head'>
-                                <li>Привiт, мене звуть</li>
-                                <li>Наталя Актиба<span className='shrift__є'>є</span>ва,</li>
-                                <li>я креативний пiдпри<span className='shrift__є'>є</span>мець</li>
-                            </ul>
-                        </li>
-                        <li id='page2__expereanse'>
-                            Майже 15 років досвіду в<br className='page2__br__adapt' />сфері маркетингу та бізнесу.<br />
-                            І це чудесна гра.<br />
-                            Якщо чогось дійсно хочу - йду до кінця.
-                        </li>
-                        <li>
-                            <ul className='page2__experience__flex'>
-                                <li>
-                                    <div className='page2__experience__old'>15</div>
-                                    років досвіду
-                                </li>
-                                <li>
-                                    <div className='page2__experience__old'>27</div>
-                                    консультацій
-                                </li>
-                                <li>
-                                    <div className='page2__experience__old'>14</div>
-                                    проектів
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
+            <div className='page2__vertical__adapt'></div>
+            <div className='page2__photo__natali'></div>
+            <div className='page2__card__border'></div>
+            <div className='page2__float__photo'>
+                <ul className='page2__profile__wrap'>
+                    <li>
+                        <ul className='page2__profile__head'>
+                            <li>Привiт, мене звуть</li>
+                            <li>Наталя Актиба<span className='shrift__є'>є</span>ва,</li>
+                            <li>я креативний пiдпри<span className='shrift__є'>є</span>мець</li>
+                        </ul>
+                    </li>
+                    <li id='page2__expereanse'>
+                        Майже 15 років досвіду в<br className='page2__br__adapt' />сфері маркетингу та бізнесу.<br />
+                        І це чудесна гра.<br />
+                        Якщо чогось дійсно хочу - йду до кінця.
+                    </li>
+                    <li>
+                        <ul className='page2__experience__flex'>
+                            <li>
+                                <div className='page2__experience__old'>15</div>
+                                років досвіду
+                            </li>
+                            <li>
+                                <div className='page2__experience__old'>27</div>
+                                консультацій
+                            </li>
+                            <li>
+                                <div className='page2__experience__old'>14</div>
+                                проектів
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
             <div className='page2__background__adapt'></div>
             <div className='page2__principles'>МОЇ  ПРИНЦИПИ</div>
