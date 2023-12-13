f07c28ba2ee7a62945a4b419d1d882eed880bad3
Владимир <davidvovchik@gmail.com>
2023-12-13 10:35:47 +0200

    qqq

--- src/components/Page3Crown/Page3Crown.js
+++ src/components/Page3Crown/Page3Crown.js
@@ -3,8 +3,9 @@ import './Page3Crown.css';
 const Page3Crown = () => {
     return (
         <div className='page3__photo__wrap'>
-            <div className='page3__border__adapt'></div>
-            <div className='page3__text'>Мiсце, де ти знайдеш iдеi<span className='shrift__point'>.</span> з запуском пiд ключ</div>
+            <div className='page3__border__adapt'>
+                <div className='page3__text'>Мiсце, де ти знайдеш iдеi<span className='shrift__point'>.</span> з запуском пiд ключ</div>
+            </div>
         </div>
     )
 }
